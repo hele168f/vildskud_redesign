77ba6340e694f4b0248470e835d3338d022b281d
Helena Nielsen <hele168f@stud.kea.dk>
2023-03-17 12:01:27 +0100

    forside

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
 import "./style.css";
+
+document.getElementById('nav-toggle').addEventListener('click', function() {
+    var navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('hidden');
+  });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        body: "#291430",
+        theme: "#FF31D8",
+        button: "#8321FF",
+        buttonHover: "#4B188B",
+      },
+
+      fontFamily: {
+        Monoton: "'Monoton', cursive;",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
