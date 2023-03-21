1da63d09cca806a7733be45cd2015cc560f89277
Helena Nielsen <hele168f@stud.kea.dk>
2023-03-21 20:27:12 +0100

    cookie boks

--- src/main.js
+++ src/main.js
@@ -26,3 +26,36 @@ function close(){
     mainMenu.style.top = '-100%';
 }
 
+// Cookie boks
+const cookieBox = document.querySelector('.cookie-box');
+const acceptAllBtn = document.querySelector('#accept-all-btn');
+const rejectAllBtn = document.querySelector('#reject-all-btn');
+const readMoreLink = document.querySelector('#read-more-link');
+
+// Function to hide the cookie box
+const hideCookieBox = () => {
+  cookieBox.style.display = 'none';
+}
+
+// Function to handle the accept all button click
+const handleAcceptAll = () => {
+  // TODO: Set all cookies to accepted
+  hideCookieBox();
+}
+
+// Function to handle the reject all button click
+const handleRejectAll = () => {
+  // TODO: Set all cookies to rejected
+  hideCookieBox();
+}
+
+// Function to handle the read more link click
+const handleReadMore = (e) => {
+  e.preventDefault();
+  // TODO: Show more information about cookies
+}
+
+// Add event listeners to the buttons and link
+acceptAllBtn.addEventListener('click', handleAcceptAll);
+rejectAllBtn.addEventListener('click', handleRejectAll);
+readMoreLink.addEventListener('click', handleReadMore);
